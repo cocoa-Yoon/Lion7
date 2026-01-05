9524bd60cba1797270b4da81a05ace35fd1a6734
Yoon Taekyu <apcocoa2019@gmail.com>
2026-01-05 23:28:39 +0900

    work

--- free_use/free_use/Program.cs
+++ free_use/free_use/Program.cs
@@ -11,9 +11,26 @@ namespace free_use
     {
         static void Main(string[] args)
         {
-            
+            int playerHealth = 5;
+            int playerStrength = 8;
+            double playerIntelligence = 7.5;
+            decimal playerLuck = 2.4m;
+            const float playerSize = 1.05f;
 
-
+            Console.WriteLine("┏━━━━━━━━━━━━━━━━  케릭터 정보 ━━━━━━━━━━━━━━━━━┓");
+            Thread.Sleep(1000);
+            Console.WriteLine($"┃          플레이어 체력              {playerHealth}         ┃");
+            Thread.Sleep(1000);
+            Console.WriteLine($"┃          플레이어 힘                {playerStrength}         ┃");
+            Thread.Sleep(1000);
+            Console.WriteLine($"┃          플레이어 지능              {playerIntelligence}       ┃");
+            Thread.Sleep(1000);
+            Console.WriteLine($"┃          플레이어 운                {playerLuck}       ┃");
+            Thread.Sleep(1000);
+            Console.WriteLine($"┃          플레이어 크기              {playerSize}      ┃");
+            Thread.Sleep(1000);
+            Console.WriteLine($"┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛");
+            Thread.Sleep(1000);
 
 
 
